73d03c07b7893340f4c611d0b9da59205849b351
yuhl <kerno1018@163.com>
2021-09-14 08:37:56 +0800

    tt

--- src/main/java/com/DemoApplication.java
+++ src/main/java/com/DemoApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class DemoApplication {
 	public static void main(String[] args) {
+		TimeZone t = TimeZone.getTimeZone("Asia/Shanghai");
+		TimeZone.setDefault(t);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
